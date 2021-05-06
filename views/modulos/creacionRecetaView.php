306305c76aaefd5898b7cdfe38ecde79f3bfa8b2
José L. Gutiérrez <gutiecuador@gmail.com>
2021-05-06 00:07:04 -0500

    fix desing

--- views/modulos/creacionRecetaView.php
+++ views/modulos/creacionRecetaView.php
@@ -171,7 +171,7 @@ $tiposDOC = $cotizacion->getVenTiposDOCWF();
                                     
                                 </tbody>
                             </table>
-                                <button type="button" @click="getComposicionProducto(documento.kit.codigo)" class="btn btn-success btn-sm"><span class="glyphicon glyphicon-search"></span> Verificar Composición</button>
+                                
                             </div>
                         </div>
                     </div>
@@ -188,6 +188,7 @@ $tiposDOC = $cotizacion->getVenTiposDOCWF();
                         <div class="panel-heading clearfix">
                         <h4 class="panel-title pull-left" style="padding-top: 7.5px; padding-bottom: 7.5px;"><i class="fa fa-list" aria-hidden="true"></i> Detalle del KIT (Composición)</h4>
                             <div class="btn-group pull-right">
+                                <button type="button" @click="getComposicionProducto(documento.kit.codigo)" class="btn btn-success btn-sm"><span class="glyphicon glyphicon-refresh"></span> Recargar Composicion</button>
                                 <button type="button"  data-toggle="modal" data-target="#modal_producto_composicion" class="btn btn-primary btn-sm"><span class="glyphicon glyphicon-search"></span> Agregar Item a Composicion</button>
                             </div>
                         </div>
