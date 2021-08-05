e4a758e1c9dd261a2f77ac75eb5f6eca837dd365
José L. Gutiérrez <gutiecuador@gmail.com>
2021-08-05 11:53:28 -0500

    init project to Adfolsa & Vitador

--- config/global.php
+++ config/global.php
@@ -1,17 +1,17 @@
 <?php
 /* Configurar aqui todas las variables globales a utilizar*/
-define("APP_NAME", "WebForms Imperium");
-define("EMPRESA_NAME", "Imperium Restaurante");
-define("APP_UNIQUE_KEY", "Imperium2021$");
+define("APP_NAME", "WebForms Adfolsa & Vitador");
+define("EMPRESA_NAME", "Adfolsa & Vitador");
+define("APP_UNIQUE_KEY", "AdfolsaVita2021$");
 define("LOGO_NAME", "./assets/img/logo.png");
-define("APP_VERSION", "2.7.0");
+define("APP_VERSION", "05.08.2021");
 define("ROOT_PATH","");   //Root del proyecto
 
 define("IMAGES_UPLOAD_DIR", $_SERVER['DOCUMENT_ROOT'].'/uploadsCotizaciones');
 
 define("VIEWS_PATH","/views");
 define("CONFIG_FILE","./config/configuraciones.xml");
-define("DEFAULT_DBName","MODELO1");
+define("DEFAULT_DBName","");
 define("DEFAULT_EMAIL","");
 
 /*Envio de correos */
@@ -20,6 +20,6 @@ define("DEFAULT_SENDER_EMAIL","");
 define("DEFAULT_EMAILPASS","");
 
 /*URL Body Email*/
-define("LOGO_ONLINE","http://www.agricolabaquero.com/img/resources/logo.png");
-define("SITIOWEB_ONLINE","http://www.agricolabaquero.com");
-define("BODY_EMAIL_TEXT","Reciba un cordial saludo de quienes conformamos IMPERIUM RESTAURANTE, estamos atendiendo a su requerimiento por lo que encontrara el documento solicitado adjunto en este correo");
+define("LOGO_ONLINE","");
+define("SITIOWEB_ONLINE","");
+define("BODY_EMAIL_TEXT","Reciba un cordial saludo de quienes conformamos".EMPRESA_NAME.", estamos atendiendo a su requerimiento por lo que encontrara el documento solicitado adjunto en este correo");
--- views/modulos/inicioView.php
+++ views/modulos/inicioView.php
@@ -7,74 +7,13 @@
     <div id="menu_circle">
       <div class='selector'>
           <ul>
-            <li onclick="location.href='?action=formularioCortes';">
-                <input id='c1' type='checkbox'/>
-                <label for='c1' title='Formulario de Cortes'><i class="fa fa-retweet fa-3x" aria-hidden="true"></i></i></label>   
-              </a>
-            </li>
-
-            <li onclick="location.href='?action=creacionReceta';">
-                <input id='c2' type='checkbox' />
-                <label for='c2' title='Creacion de receta'><i class="fa fa-cutlery fa-3x" aria-hidden="true"></i></i></label>   
-              </a>
-            </li>
-          
+            
             <li onclick="location.href='?action=cotizaciones';">
                 <input id='c3' type='checkbox' />
                 <label for='c3' title='Punto de Venta - Cotizaciones'><i class="fa fa-shopping-cart fa-3x" aria-hidden="true"></i></i></label>   
               </a>
             </li>
 
-            <li onclick="location.href='?action=costoTeorico';">
-                <input id='c4' type='checkbox' />
-                <label for='c4' title='Costo Teórico'><i class="fa fa-area-chart fa-3x" aria-hidden="true"></i></i></label>   
-              </a>
-            </li>
-          
-
-<!-- 
-            <li onclick="location.href='/wssp/ws-admin/';">
-              <input id='c4' type='checkbox'/>
-            <label id='menu4' for='c4' title='Administración'></label>
-            </li>
-
-            <li onclick="location.href='/wssp/ws-evanfitriones/';">
-              <input id='c5' type='checkbox'/>
-            <label id='menu5' for='c5' title='Registros de Anfitriones'></label>
-            </li>
-
-            <li onclick="location.href='/wssp/ws-evaluagge/';">
-              <input id='c6' type='checkbox'/>
-            <label id='menu6' for='c6' title='Evaluación de Jefes'></label>
-            </li>
-
-            <li onclick="location.href='../intranetApp-v2.1';">
-              <input id='c7' type='checkbox'/>
-              <label id='menu7' for='c7' title='Mantenimiento de Equipos'></label>
-            </li>
-
-            <li onclick="location.href='/wssp/ws-estadovehiculo/ordenPedido.php';">
-              <input id='c8' type='checkbox'/>
-              <label id='menu8' for='c8' title='Orden de Pedido (Vehiculos)'></label>   
-            </li>
-
-            <li onclick="location.href='/wssp/ws-estadovehiculo/';">
-              <input id='c9' type='checkbox'/>
-              <label id='menu9' for='c9' title='Estado del Vehiculo'></label>   
-            </li>
-
-            <li onclick="location.href='../carteraclientes';">
-              <input id='c10' type='checkbox'/>
-              <label id='menu10' for='c10' title='Cartera de Clientes'></label>   
-            </li>
-
-            <li onclick="location.href='../cotizacionesApp';">
-              <input id='c11' type='checkbox'/>
-              <label id='menu11' for='c11' title='Cotizaciones Web'></label>   
-            </li>
-
- -->
-           
           </ul>
           <button id='center_logo'></button>
           
